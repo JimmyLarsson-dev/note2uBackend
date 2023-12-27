02c754f22c1c118c07916089540c28419c7aa0f9
Jimmy Larsson <jimmy.larsson@europe.com>
2023-12-27 17:25:24 +0100

    Commenting out itemList fixes problem. Now to find out how to properly it.

--- src/main/java/com/example/note2ubackendnosecurity/checklist/ChecklistEntity.java
+++ src/main/java/com/example/note2ubackendnosecurity/checklist/ChecklistEntity.java
@@ -4,42 +4,44 @@ import com.example.note2ubackendnosecurity.user.UserEntity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
 import java.util.List;
 import java.util.UUID;
 
 @Entity
-//@AllArgsConstructor
-//@NoArgsConstructor
-@RequiredArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "checklist")
 public class ChecklistEntity {
 
-
-//    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name= "checklist_id")
-    private Long id;
+    UUID id;
     private String title;
-    @ManyToMany()
-    @Cascade({org.hibernate.annotations.CascadeType.REFRESH, org.hibernate.annotations.CascadeType.MERGE, org.hibernate.annotations.CascadeType.PERSIST})
-    private List<Item> itemList;
+
+//    @ManyToMany()
+//    @Cascade({org.hibernate.annotations.CascadeType.REFRESH, org.hibernate.annotations.CascadeType.MERGE, org.hibernate.annotations.CascadeType.PERSIST})
+//    private List<Item> itemList;
     @ManyToMany
-    @Cascade({ org.hibernate.annotations.CascadeType.SAVE_UPDATE, org.hibernate.annotations.CascadeType.MERGE, org.hibernate.annotations.CascadeType.PERSIST})
+    @Cascade({CascadeType.SAVE_UPDATE, CascadeType.MERGE, CascadeType.PERSIST})
     @JoinColumn(name = "userId")
     private List<UserEntity> users;
     private boolean statusBeenViewed;
 
 
-    public ChecklistEntity(Long id,
+    public ChecklistEntity(/*UUID id,*/
                            String title,
+/*
                            List<Item> itemList,
+*/
                            UserEntity user,
                            boolean statusHasBeenViewed) {
-        this.id = id;
+        /*this.id = id;*/
         this.title = title;
-        this.itemList = itemList;
+        /*this.itemList = itemList;*/
         this.users = List.of(user);
         this.statusBeenViewed = statusHasBeenViewed;
 
